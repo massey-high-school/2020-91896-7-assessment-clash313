16a3615f0804b466dc2aa5b5c404cf98257fbc69
clash313 <60630460+clash313@users.noreply.github.com>
2020-07-27 13:19:51 +1200

    added

---
+++ 01_num_checker.py
@@ -0,0 +1,24 @@
+# Functions go here
+
+def num_checker(question):
+
+    error = "it should contain greater numbers than zero"
+
+    valid = False
+    while not valid:
+        response = input(question)
+        has_errors = ""
+
+        # look at each character in string and if it's a number, complain
+        for letter in response:
+            if letter.isdigit():
+                has_errors = "yes"
+                break
+
+        if response == "":
+            continue
+        elif has_errors != "":
+            print(error)
+            continue
+        else:
+            return response
